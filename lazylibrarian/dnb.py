3cf09ee67fa5c8066ff263663a05c096a5c81138
Corey Ward <157808172+cdude5464@users.noreply.github.com>
2026-06-13 19:18:23 -0400

    Handle legacy iso639 fallback

--- lazylibrarian/dnb.py
+++ lazylibrarian/dnb.py
@@ -20,7 +20,7 @@ import requests
 # The new library is recently updated and uses Lang
 try:
     from iso639 import Lang
-except ModuleNotFoundError:
+except (ImportError, ModuleNotFoundError):
     Lang = None
 try:
     from iso639 import languages
